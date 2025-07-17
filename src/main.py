94bb6bdb68cc7fdb2588b6e1228709b0aeb78166
Evert Ramos <evert.ramos@gmail.com>
2025-07-17 10:28:55 -0300

    Separar arquivos e usar aspas simples

--- src/main.py
+++ src/main.py
@@ -2,24 +2,18 @@
 # Written by Evert Ramos
 
 # Importa funções externas
-from utils.print import sucesso, erro, aviso, padrao
-
-# Função de input colorido
-def inputc(text, color_code):
-    return input(f"\033[{color_code}m{text}\033[0m")
-def pergunta(text):
-    return inputc(text, "94")  # Azul
-
+from utils.print import sucesso, erro, aviso, padrao, pergunta
 
 # Escolher os jogos
 def escolher_jogos():
-    jogos = ["Mega", "Quina"]
-    padrao("Escolha um jogo:")
+    jogos = ['Mega', 'Quina']
+    padrao('Escolha um jogo:')
     for i, jogo in enumerate(jogos, start=1):
-        padrao(f"{i}. {jogo}")
-    padrao("0. Voltar ao menu")
+        padrao(f'{i}. {jogo}')
+    padrao('0. Voltar ao menu')
+    padrao('00. Sair do programa')
     try:
-        escolha_jogo = int(pergunta("Digite o número do jogo escolhido: "))
+        escolha_jogo = int(pergunta('Digite o número do jogo escolhido: '))
         print()
         match escolha_jogo:
             case 0:
@@ -29,39 +23,45 @@ def escolher_jogos():
             case 2:
                 quina()
             case _:
-                erro("Opção inválida, por favor escolha uma das opoções indicadas.")
+                erro('Opção inválida, por favor escolha uma das opoções indicadas.')
                 return escolher_jogos()
     except ValueError:
-        erro("Entrada inválida, por favor insira um número.")
+        erro('Entrada inválida, por favor insira um número.')
         return escolher_jogos()
-    print("---fim [Jogar]---")
+    print('---fim [Jogar]---')
 
 # Jogo Mega
 def mega():
     qtd_numeros = 6
     numeros = []
-    print("Iniciando o jogo Mega...")
+    padrao('')
+
+    padrao('00. Sair do programa')
+
+    print('Iniciando o jogo Mega...')
+
+
 
 # Jogo Quina
 def quina():
     qtd_numeros = 5
     numeros = []
-    print("Iniciando o jogo Quina...")
+    print('Iniciando o jogo Quina...')
 
 # Menu da aplicação
 while True:
     print()
-    padrao("Menu:")
-    padrao("1. Jogar")
-    padrao("2. Opção 2")
-    padrao("0. Sair")
+    padrao('Menu:')
+    padrao('1. Jogar')
+    padrao('2. Opção 2')
+    padrao('0. Sair')
 
     try:
-        escolha = int(pergunta("Escolha uma opção: "))
-        print("---fim [Menu]---")
+        escolha = int(pergunta('Escolha uma opção: '))
+        print('---fim [Menu]---')
         print() 
     except ValueError:
-        erro("Entrada inválida, por favor insira um número.")
+        erro('Entrada inválida, por favor insira um número.')
         continue
 
     if escolha == 0:
@@ -69,8 +69,8 @@ while True:
     elif escolha == 1:
         escolher_jogos()
     elif escolha == 2:
-        print("Você escolheu a Opção 2.")
+        print('Você escolheu a Opção 2.')
     else:
-        print("Opção inválida, tente novamente.")
+        print('Opção inválida, tente novamente.')
     
 print('Programa encerrado.')
